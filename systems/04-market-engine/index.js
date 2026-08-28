9d428bd856efb5572d3938c96edfd496a24c954c
anyesgray38 <anyesgray38@gmail.com>
2026-08-28 09:18:27 -0400

    Expose market engine integration boundary

---
+++ systems/04-market-engine/index.js
@@ -0,0 +1,3 @@
+export { INSTRUMENTS, SESSIONS } from './instruments.js';
+export { createRng, nextBar, generateBars } from './market-engine.js';
+export { advanceMarket, getPrices } from './market-transition.js';
