abf1df6f3c4c695f39a01f1f822eca8ecd5558e6
anyesgray38 <anyesgray38@gmail.com>
2026-08-28 09:35:20 -0400

    Expose progression domain boundary

---
+++ systems/01-state/index.js
@@ -0,0 +1,4 @@
+export { createInitialState, cloneState } from './state.js';
+export { awardXp, calculateLevel, xpForNextLevel, createProgressionState } from './progression.js';
+export { availableUnlocks, applyUnlocks } from './unlocks.js';
+export { processProgression } from './progression-engine.js';
